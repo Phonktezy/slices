33821e615ea3486b7feb3cd66ec6c13f55c53f0e
Phonktezy <144049246+Phonktezy@users.noreply.github.com>
2023-10-05 12:17:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+# s = input()
+# print(s[2])
+# print(s[-2])
+# print(s[:5])
+# print(s[:-2])
+# print(s[0:-2])
+# print(s[1::2])
+# print(s[::-1])
+# print(s[::-2])
+# print(len(s))
+# print(s.count)
+# word = input()
+# middle = len(word) // 2
+# print(middle)
+# new_word = word[middle:] + word[:middle]
+# print(new_word)s
+# first_word = input("Введите слово: ")
+# find_letter = first_word.find('f')
+# find_letterf = first_word.rfind('f')
+# if find_letter != -1:
+#     if find_letterf == find_letter:
+#         print("Первая инициируемая буква найдена: ", find_letter)
+#     else:
+#         print("Буква в начале 'f':", find_letter)
+#         print("Буква в конце 'f':", find_letterf)
+# s = input("Введите слово: ")
+# a = s.find('h')
+# b = s.rfind('h') + 1
+# print("Результат: ", s[:a] + s[b:])
+# s = input("Введите номер: ")
+# s = s.replace('1','one')
+# print("Результат: ", s)
+# s = input()
+# s = s.replace('@', '')
+# print(s)
+#
+# s = input()
+# a = s[:s.find('h') + 1]
+# b = s[s.find('h') + 1:s.rfind('h')]
+# c = s[s.rfind('h'):]
+# print(a + b.replace('h', 'H') + c)
+
+
+
+
